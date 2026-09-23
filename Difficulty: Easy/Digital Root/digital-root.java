e9b3b5f41c6dd0be05579d11104608f6a64776d5
ramavath-bunny-naik <ramavath.bunny2007@gmail.com>
2026-09-23 21:54:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Digital Root/digital-root.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int digitalRoot(int n) {
+
+
+      while(n>9) {
+          int sum=0;
+          while(n>0) {
+              sum=sum+n%10;
+              n=n/10;
+          }
+          n=sum;
+      }
+
+      return n; 
+    }
+}
